51449e9dba762b4d50e3b28910c0ddeb105e38a2
987102818@qq.com <987102818@qq.com>
2016-03-09 20:42:38 +0800

    second version

--- projects/10/CompilationEngine.py
+++ projects/10/CompilationEngine.py
@@ -1,11 +1,38 @@
+import JackTokenizer
+
+
 class CE:
 
 	def __init__(self):
 		self.tokenDict = { '(':'(', ')':')', '[':'[', ']':']' }
+		self.output
+		self.keyword_pre = '<keyword>'
+		self.keyword_post = '</keyword>'
 
 	def CompileClass(self):
 
-	def CompileClassVarDec(self):
+	def CompileClassVarDec(self, token):
+		if token in ['static', 'field']:
+			self.output.write( self.keyword_pre + token + self.keyword_post )
+			nextToken = self.getToken()
+		else:
+			print('error')
+
+
+	def CompileType(self, token):
+		if token in ['int', 'char', 'boolean']:
+			self.output.write( self.keyword_pre + token + self.keyword_post )
+		elif JackTokenizer().isIdentifier( token ) == True:
+			self.output.write( self.keyword_pre + token + self.keyword_post )
+		else:
+			print('error')
+
+
+
+
+
+
+
 
 	def CompileSubroutine(self):
 
@@ -38,7 +65,119 @@ class CE:
 		else:
 			print('Unmatched error')
 
+
+	"Get the next token from the sequnce of tokens"
+	def getToken(self, seq):
+
+
+class isClass:
+
+	#def __init__(self):
+
+	def isClass(self, token):
+		if token == 'class':
+			return True
+		else:
+			return False
+
+	def isClassVarDec(self, token):
+		if token in [ 'static', 'field' ]:
+			return True
+		else:
+			return False
+
+	def isType(self, token):
+		if token in [ 'int', 'char', 'boolean' ]:
+			return True
+		elif JackTokenizer().isIdentifier( token ) == True:
+			return True
+		else:
+			return False
+
+	def isSubroutineDec(self, token):
+		if token in [ 'constructor', 'function', 'method' ]:
+			return True
+		else:
+			return False
+
+	def isParameterList(self, token):
+		next = self.getToken()
+		if self.isType( token ) and JackTokenizer().isIdentifier( next ):
+			return True
+		else:
+			return False
+
+	def isSubroutineBody(self, token):
+		next = self,getToken()
+		if token == '{' and self.isVarDec( next ):
+			return True
+		else:
+			return False
+
+	def isVarDec(self, token):
+		if token == 'var':
+			return True
+		else:
+			return False
+
+	def isClassName(self, token):
+		if JackTokenizer().isIdentifier( token ):
+			return True
+		else:
+			return False
+
+	def isSubroutineName(self, token):
+		if JackTokenizer().isIdentifier( token ):
+			return True
+		else:
+			return False
+
+	def isVarName(self, token):
+		if JackTokenizer().isIdentifier( token ):
+			return True
+		else:
+			return False
+
+class isStatement:
+
+	def isLetStatement(self, token):
+		if token == 'let':
+			return True
+		else:
+			return False
+
+	def isIfStatement(self, token):
+		if token == 'if':
+			return True
+		else:
+			return False
+
+	def isWhileStatement(self, token):
+		if token == 'while':
+			return True
+		else:
+			return False
+
+	def isDoStatement(self, token):
+		if token == 'do':
+			return True
+		else:
+			return False
+
+	def isReturnStatement(self, token):
+		if token == 'return':
+			return True
+		else:
+			return False
+
+
+class isExpression:
+
 	def 
+
+
+
+
 
 
 
