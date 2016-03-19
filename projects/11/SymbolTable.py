98851b74e2a066f7ea68c015517bd7cc8fc79d60
987102818@qq.com <987102818@qq.com>
2016-03-19 23:27:50 +0800

    1st version of symbol table

---
+++ projects/11/SymbolTable.py
@@ -0,0 +1,28 @@
+class SymbolTable():
+
+	def __init__(self):
+		self._symbol_table = {}
+
+	# Starts a new subroutine scope
+	def startSubroutine(self):
+
+	# Defines a new identifier of a given name, type, and kind and 
+	# assigns it a running index. STATIC and FIELD identifiers have
+	# a class scope, while ARG and VAR identifier have a subroutine 
+	# scope
+	def Define(self, name, type, kind):
+
+	# Returns the number of variables of the given kind already defined
+	# in the current scope
+	def VarCount(self, kind):
+
+	# Returns the kind of the named identifier in the current scope.
+	# If the identifier is unknown in the current scope, returns None.
+	def KindOf(self, name):
+
+	# Returns the type of the named identifier in the current scope.
+	def TypeOf(self, name):
+
+	# Returns the index assigned to the named identifier.
+	def IndexOf(self, name):
+		
